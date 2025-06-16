73aea1660c2e14ace79709c33d229b8bb8fb2442
yjx159 <3294369792@qq.com>
2025-06-16 22:18:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var num1=20;
+var num2=30;
+str="HelloWorld";
+  var str1=str.slice(0,6);
+  console.log(str1);
+  str1=str.concat("JavaScript");
+console.log(str1);
+
+  console.log(Array.isArray(str1));
+  function add(a,b){
+  console.log("Adding two numbers");
+  
+    return a+b;
+  }
